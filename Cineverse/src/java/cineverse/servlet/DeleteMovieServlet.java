e529c5bce070b6b3eccf05d850ea06ef7ef06396
Chamith Harshamal <chamithweerasinghe1@gmail.com>
2025-01-22 16:45:01 +0530

    Create DeleteMovieServlet.java

---
+++ Cineverse/src/java/cineverse/servlet/DeleteMovieServlet.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cineverse.servlet;
+
+import cineverse.dao.MovieDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/admin/DeleteMovieServlet")
+public class DeleteMovieServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        try {
+            int movieId = Integer.parseInt(request.getParameter("movieId"));
+            MovieDAO movieDAO = new MovieDAO();
+            boolean isDeleted = movieDAO.deleteMovie(movieId);
+
+            if (isDeleted) {
+                session.setAttribute("message", "Movie deleted successfully!");
+            } else {
+                session.setAttribute("message", "Failed to delete movie!");
+            }
+        } catch (Exception e) {
+            session.setAttribute("message", "Error: " + e.getMessage());
+        }
+
+        response.sendRedirect(request.getContextPath() + "/admin/addMovie.jsp");
+    }
+}
